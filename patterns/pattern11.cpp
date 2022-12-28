c6da96907ecc95b075d21b9af8c094e4ee92d06b
Sana Asif <sana98asif@gmail.com>
2022-12-28 22:30:11 +0530

    My repo.

---
+++ patterns/pattern11.cpp
@@ -0,0 +1,31 @@
+//Pattern 11.
+//*    
+//**   
+//***  
+//**** 
+//*****
+//**** 
+//***
+//**
+//*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; 
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
