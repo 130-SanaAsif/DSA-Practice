84a479cd9c4ea96ebae95bfcebab49388138db32
Sana Asif <sana98asif@gmail.com>
2023-01-03 22:01:55 +0530

    My repo.

---
+++ array-ques/remove_duplicate_element.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int removeDuplicate(int arr[],int n){
+    if(n==0){
+        return 0;
+    }
+    int i=0;
+    for(int j=1; j<n; j++){
+        if(arr[i] != arr[j]){
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i+1;
+}
+
+int main(){
+    int arr[] = {1,2,2,2,5,5,5,5,5,5,5,7,7,7,8,8,8,8,9,9,20,20};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = removeDuplicate(arr,n);
+    for(int i=0; i<k; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
