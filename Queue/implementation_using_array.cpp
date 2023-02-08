600273adad13320be4d53ea1d1886a1d06832c1b
Sana Asif <sana98asif@gmail.com>
2023-02-09 02:14:34 +0530

    My repo.

---
+++ Queue/implementation_using_array.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+#define n 100
+
+class queue{
+    int *arr;
+    int front;
+    int back;
+    public:
+        queue(){
+            front = -1;
+            back = -1;
+            arr = new int[n]; 
+        }
+        void push(int x){
+            if(back == n-1){
+                cout<<"Queue OverFlow"<<endl;
+                return;
+            }
+            back++;
+            arr[back] = x;
+            
+            if(front == -1){
+                front++;
+            }
+        }
+
+        void pop(){
+            if(front == -1 || front > back){
+                cout<<"No elements in queue"<<endl;
+                return;
+            }
+            front++;
+        }
+
+        int peek(){
+            if(front == -1 || front > back){
+                cout<<"No elements in queue"<<endl;
+                return -1;
+            }
+            return arr[front];
+        }
+
+        bool empty(){
+            if(front == -1 || front > back){
+                return true;
+            }
+            return false;
+        }
+};
+
+int main(){
+    queue q;
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    q.push(8);
+    cout<<"Element present in queue: "<<endl;
+    while(!q.empty()){
+        cout<<q.peek()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+    if(q.empty()){
+        cout<<"True";
+    }
+    else{
+        cout<<"Flase";
+    }
+    return 0;
+}
