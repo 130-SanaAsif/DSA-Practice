b97e45e0a1cb7654b622239e8348d724f6f43aa2
Sana Asif <sana98asif@gmail.com>
2023-02-24 23:58:44 +0530

    My repo.

---
+++ patterns/pascalTriangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void pascalTriangle(int n){
+    int arr[n][n];
+    for(int l=0; l<n; l++){
+        for(int i = 0; i<= l; i++){
+            if(l == i || i == 0)
+                arr[l][i] = 1;
+            else
+                arr[l][i] = arr[l - 1][i - 1] +
+                            arr[l - 1][i];
+        cout << arr[l][i] << " ";
+        }
+        cout << "\n"; 
+    }
+}
+
+int main(){
+    int n= 5;
+    pascalTriangle(n);
+    return 0;
+}
