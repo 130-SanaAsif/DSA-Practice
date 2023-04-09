1d6232c8ad5c99c102617448234e805ab26cb1bd
Sana Asif <sana98asif@gmail.com>
2023-04-09 23:31:20 +0530

    MyRepo.

--- GFG POTD/8April.cpp
+++ GFG POTD/8April.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//using vextor.
 vector<int> makeBeautiful(vector<int>&arr){
     vector<int>ans;
     for(int i=0; i<arr.size(); i++){
@@ -18,11 +18,35 @@ vector<int> makeBeautiful(vector<int>&arr){
     }
     return ans;
 }
-
+//using stack.
+vector<int> beautifulWithStack(vector<int>&arr){
+    stack<int>s1;
+    for(int i=0; i<arr.size(); i++){
+        if(s1.empty()){
+            s1.push(arr[i]);
+        }
+        else{
+            if((s1.top()>=0 && arr[i]<0) || (s1.top()<0 && arr[i]>=0)){
+                s1.pop();
+            }
+            else{
+                s1.push(arr[i]);
+            }
+        }
+    }
+    vector<int>ans;
+    while(!s1.empty()){
+        ans.push_back(s1.top());
+        s1.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
 int main(){
-    vector<int>arr = {4,2,-2,1};
+    vector<int>arr = {4,3,2,-2,-1,5};
     vector<int>res;
-    res = makeBeautiful(arr);
+    //res = makeBeautiful(arr);
+    res = beautifulWithStack(arr);
     for(int i=0; i<res.size();i++){
         cout<<res[i]<<" ";
     }
