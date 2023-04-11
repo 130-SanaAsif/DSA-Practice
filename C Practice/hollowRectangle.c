dd4e677fb74479db508b4b96e3250f8db0589e23
Sana Asif <sana98asif@gmail.com>
2023-04-11 23:37:58 +0530

    MyRepo.

--- C Practice/hollowRectangle.c
+++ C Practice/hollowRectangle.c
@@ -1,23 +1,28 @@
-#include<stdio.h>
-int main(){
-    int n,m;
+#include <stdio.h>
+int main()
+{
+    int n, m;
     printf("Enter length of the rectangle : ");
     scanf("%d", &n);
     printf("Enter width of the rectangle : ");
     scanf("%d", &m);
 
-    for(int i=1; i<=n; i++){
+    for (int i = 1; i <= n; i++)
+    {
 
-        for(int j=1; j<=m; j++){
+        for (int j = 1; j <= m; j++)
+        {
 
-            if(i==1 || i==n || j==1 || j==m){
+            if (i == 1 || i == n || j == 1 || j == m)
+            {
                 printf("*");
             }
-            else{
+            else
+            {
                 printf(" ");
             }
         }
         printf("\n");
     }
-return 0;
+    return 0;
 }
