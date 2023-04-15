e0b54f0c12ffc5271be12174cbf26da3efdcc8f5
Sana Asif <sana98asif@gmail.com>
2023-04-15 23:52:41 +0530

    MyRepo.

---
+++ GFG POTD/15April.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int finLength(vector<int>&color, vector<int>&arr, int n){
+
+}
+
+int main(){
+    int n=3;
+    vector<int>arr = {2,2,5};
+    vector<int>brr = {3,3,4};
+
+}
