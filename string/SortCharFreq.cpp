f8d0514dbb61f6884073a65015620e1858774805
Sana Asif <sana98asif@gmail.com>
2023-05-17 23:34:34 +0530

    My repo.

--- string/SortCharFreq.cpp
+++ string/SortCharFreq.cpp
@@ -2,18 +2,30 @@
 using namespace std;
 
 string sortCharFreq(string s){
-    string res = "";
-    int cnt = 0;
-    for(int i=0; i<s.size(); i++){
-        if(s[i] == s[i+1]);
-            cnt++;
-    }
     
+    unordered_map<char,int>mp;
+    priority_queue<pair<int,char>>pq;
+    string ans = "";
+    for(int i=0; i<s.size(); i++){
+        mp[s[i]]++;
+    }
+    for(auto it:mp){
+        pq.push({it.second,it.first});
+    }
+    while(!pq.empty()){
+        auto temp = pq.top();
+        int freq = temp.first;
+        char ch = temp.second;
+
+        ans+=string(freq,ch);
+        pq.pop();
+    }
+    return ans;
 }
 
 int main(){
-    string s = "tree";
-    string res = sortCharFreq(s);
-    cout<<res<<endl;
+    string s = "Aabb";
+    string ans = sortCharFreq(s);
+    cout<<ans;
     return 0;
 }
