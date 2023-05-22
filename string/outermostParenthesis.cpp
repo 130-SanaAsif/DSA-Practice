5c43c428775cec6182e4d6e486325220b615e095
Sana Asif <sana98asif@gmail.com>
2023-05-22 23:11:54 +0530

    My repo.

--- string/outermostParenthesis.cpp
+++ string/outermostParenthesis.cpp
@@ -6,22 +6,16 @@ string outerParenthesis(string s){
     int cnt = 0;
     for(int i=0; i<s.size(); i++){
         if(s[i] == '('){
-            cout<<i<<" "<<"1st condition"<<endl;;
             cnt++;
-            cout<<cnt<<" "<<endl;
         }
         else{
-            cout<<i<<" second condition"<<endl;
             cnt--;
-            cout<<cnt<<" "<<endl;
         }
         if((s[i] == '(' && cnt == 1) || (s[i] == ')' && cnt == 0)){
-            cout<<i<<" 3rd condition"<<endl;
-            cout<<cnt<<" "<<endl;
             continue;
         }
         res.push_back(s[i]);
-        cout<<"pushed "<<endl;
+
     }
     return res;
 }
