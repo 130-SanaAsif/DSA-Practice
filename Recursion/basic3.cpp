3699b10c07bcf35773c9f698a73b387b9ac87204
Sana Asif <sana98asif@gmail.com>
2023-05-27 13:49:15 +0530

    My repo.

--- Recursion/basic3.cpp
+++ Recursion/basic3.cpp
@@ -9,10 +9,11 @@ void print(int n){
         cout<<n<<endl;
         return print(n-1);
     }
-}
+}  
 
 int main(){
     int n;
+    cout<<"Enter a number: ";
     cin>>n;
     print(n);
     return 0;
