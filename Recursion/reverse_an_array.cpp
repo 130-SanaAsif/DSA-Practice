e9b32824ce696731acc95b87a43cc80a295177a8
Sana Asif <sana98asif@gmail.com>
2023-05-30 23:18:38 +0530

    My repo.

--- Recursion/reverse_an_array.cpp
+++ Recursion/reverse_an_array.cpp
@@ -8,24 +8,30 @@ void printarray(int arr[], int n){
     cout<<endl;
 }
 
-//void ReverseArray(int arr[], int n, int i){
-//    if(i >= n/2){
-//        return;
-//    }
-//    swap(arr[i], arr[n-i-1]);
-//    return ReverseArray(arr,n,i+1);
-//}
-void reverseArray(int arr[], int n){
-    
-    return reverse(arr, arr + n);
+void ReverseArray(int arr[], int n, int i){
+    if(i >= n/2){
+        return;
+    }
+    swap(arr[i], arr[n-i-1]);
+    return ReverseArray(arr,n,i+1);
 }
+//void reverseArray(int arr[], int n){
+//    
+//    return reverse(arr, arr + n);
+//}
+//void reverseArray(int arr[], int lo, int hi){
+//    if(lo >= hi)
+//        return;
+//    swap(arr[lo],arr[hi]);
+//    reverseArray(arr,lo+1, hi-1);
+//}
 int main(){
     int arr[] = {1,2,3,4,5,6};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout<<"Before Reversing:"<<endl;
     printarray(arr,n);
     cout<<"After Reversing:"<<endl;
-    reverseArray(arr,n);
+    ReverseArray(arr,n,0);
     printarray(arr,n);
     return 0;
 }
