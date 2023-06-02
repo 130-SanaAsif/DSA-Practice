15e22cae23ac289dae717e9e3ebad6d40f6e2010
Sana Asif <sana98asif@gmail.com>
2023-06-02 22:49:51 +0530

    My repo.

---
+++ Recursion/subsequenceArray.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void SubArray(int ind, vector<int>&ds, int arr[], int n){
+    if(ind == n){
+        for(int i=0; i<ds.size(); i++){
+            cout<<ds[i]<<" ";
+        }
+        if(ds.size() == 0){
+            cout<<"{}";
+        }
+        cout<<endl;
+        return;
+    }
+    //Take.
+    ds.push_back(arr[ind]);
+    SubArray(ind+1, ds, arr, n);
+    ds.pop_back();
+    //Non-Take.
+    SubArray(ind+1, ds, arr, n);
+}
+
+int main(){
+    int arr[] = {3,1,2};
+    int n = 3;
+    vector<int>ds;
+    SubArray(0,ds,arr,n);
+    return 0;
+}
