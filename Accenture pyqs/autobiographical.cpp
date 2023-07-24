e46a8dc1d381488f50a44ab37f60459fc07ffec6
Sana Asif <sana98asif@gmail.com>
2023-07-24 22:51:44 +0530

    My repo.

--- Accenture pyqs/autobiographical.cpp
+++ Accenture pyqs/autobiographical.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 int autobiographicString(string s){
-    if(s == NULL){
+    int sum = 0;
+    set<char>st;
+
+    for(int i=0; i<s.size(); i++){
+        sum += (s[i] -'0');
+        st.insert(s[i]);
+    }
+    if(sum != s.size()){
         return 0;
     }
-    set<char>res;
-    for(int i=0; i<=s.size(); i++){
+    return st.size();
+}
 
-    }
+int main(){
+    // string n = "6210001000";
+    string n = "1210";
+    int res = autobiographicString(n);
+    cout<<res<<endl;
     return 0;
 }
---
+++ Accenture pyqs/rat_count_house.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ratCount(int r, int unit, int n, int arr[]){
+    if(n == 0){
+        return -1;
+    }
+
+    int require = r*unit;
+    int sumFood = 0;
+    int i;
+    for(i=0; i<n; i++){
+        sumFood += arr[i];
+        if(sumFood >= require){
+            break;
+        }
+    }
+
+    if(sumFood < require){
+        return 0;
+    }
+    
+    return i+1;
+}
+
+int main(){
+    int arr[] = {2,8,3,5,7,4,1,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int res = ratCount(7,2,n,arr);
+    cout<<res<<endl;
+    return 0;
+}
