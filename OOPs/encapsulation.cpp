ff095c9410a8e384512b127173bd85337022af53
Sana Asif <sana98asif@gmail.com>
2023-11-28 19:09:46 +0530

    My repo.

--- OOPs/encapsulation.cpp
+++ OOPs/encapsulation.cpp
@@ -1,26 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class student{
-private:
-    string studentName;
-    int age;
-    int rollno;
+// class student{
+// private:
+//     string studentName;
+//     int age;
+//     int rollno;
 
+// public:
+//     string getName(){
+//         return this->studentName;
+//     }
+//     int getAge(){
+//         return this->age;
+//     }
+//     int getRollno(){
+//         return this->rollno;
+//     }
+// };
+
+class temp{
+private:
+    int a;
+    int b;
 public:
-    string getName(){
-        return this->studentName;
-    }
-    int getAge(){
-        return this->age;
-    }
-    int getRollno(){
-        return this->rollno;
+    int solve(int x){
+        a = x;
+        b = a/2;
+        return b;
     }
 };
 
 int main(){
-    student s;
-    
+    // student s;
+    int x;
+    cin>>x;
+    temp half;
+    int ans = half.solve(x);
+    cout<<ans;
 return 0;
 }
