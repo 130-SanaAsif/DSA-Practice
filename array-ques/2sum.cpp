6796b9d34f0659dde33b3e2b234a050efbc8a053
Sana Asif <sana98asif@gmail.com>
2024-01-16 23:55:46 +0530

    My repo.

---
+++ array-ques/2sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>bruteForce(vector<int>arr, int target){
+    
+    for(int i=0;i<arr.size(); i++){
+        for(int j=i;)
+    }
+}
+
+int main(){
+vector<int>arr = {2,7,11,15};
+
+return 0;
+}
