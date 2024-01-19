3d3b9dd11fa736620d7a98596eaafc3a6ae81073
Sana Asif <sana98asif@gmail.com>
2024-01-19 23:41:27 +0530

    My repo.

---
+++ array-ques/set_matrix.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void setMatrix(vector<vector<int>>&mat){
+    int n = mat.size();
+    int m = mat[0].size();
+
+    for(int i=0; i<n; i++)
+}
+
+int main(){
+
+return 0;
+}
