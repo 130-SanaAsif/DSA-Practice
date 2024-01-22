6a88a00d1acbac8cef009667109c8d6c9f9be48d
Sana Asif <sana98asif@gmail.com>
2024-01-22 23:54:30 +0530

    My repo.

--- array-ques/set_matrix.cpp
+++ array-ques/set_matrix.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Brute Force.
 void markRow(vector<vector<int>>&mat, int m, int i){
     for(int j=0; j<m; j++){
         if(mat[i][j] != 0){
@@ -39,13 +40,40 @@ void setMatrix(vector<vector<int>>&mat){
     }
 }
 
+//Better Approach.
+void setMatrix2(vector<vector<int>>&mat){
+    int n = mat.size();
+    int m = mat[0].size();
+
+    int row[n] = {0};
+    int col[m] = {0};
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(mat[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(row[i] == 1 || col[j] == 1){
+                mat[i][j] = 0;
+            }
+        }
+    }
+}
+
 int main(){
 vector<vector<int>>mat = {{1,1,1,1},
                           {1,0,0,1},
                           {1,1,0,1},
                           {1,1,1,1}};
 
-setMatrix(mat);
+//setMatrix(mat);
+setMatrix2(mat);
 for(int i=0; i<mat.size(); i++){
     for(int j=0; j<mat[0].size(); j++){
         cout<<mat[i][j]<<" ";
