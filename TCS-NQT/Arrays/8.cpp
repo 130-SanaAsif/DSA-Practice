00ee1df77a0ec9d694f47a65e252090807a19fd9
Sana Asif <sana98asif@gmail.com>
2024-02-21 01:15:27 +0530

    My repo.

--- TCS-NQT/Arrays/8.cpp
+++ TCS-NQT/Arrays/8.cpp
@@ -16,15 +16,44 @@ void swap(int arr[], int a, int b, int k){
     }
 }
 
+void blockSwap(int arr[], int k, int n){
+    if (k == 0 || k == n)
+        return;
+    // If first part and second part are of same size
+    if (k == n - k)
+    {
+        swap(arr, 0, n - k, k);
+        return;
+    }
+    // If first part of array is of smaller size
+    else if (k < n - k)
+    {
+        swap(arr, 0, n - k, k);
+        blockSwap(arr, k, n - k); // second part of array is taken now
+    }
+    // If second part of array is of smaller size
+    else
+    {
+        swap(arr, 0, k, n - k);
+        blockSwap(arr + n - k, 2 * k - n, k);
+    }
+}
+
 int main(){
     int n,k;
     int arr[n];
 
     cin>>n;
+    cout<<"Value of k = ";
+    cin>>k;
+
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    cin>>k;
+    
+    printArray(arr, n);
+    blockSwap(arr, k, n);
+    printArray(arr, n);
 
 return 0;
 }
