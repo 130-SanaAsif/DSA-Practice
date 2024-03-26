4bb23d98be12d56a927778bd2827aefeb0923222
Sana Asif <sana98asif@gmail.com>
2024-03-26 21:27:57 +0530

    My repo.

--- 100Days/day3a.cpp
+++ 100Days/day3a.cpp
@@ -1,40 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long reverseDigit(long long n){
-    long long rev = 0;
-    while(n--){
-        rev = rev*10 + n%10;
-        n = n/10;
-    }
-    return rev;
-}
-
-bool checkPudding(long long n){
-    long long ori = n;
-
-    long long rev1 = reverseDigit(n);
-    long long rev2 = reverseDigit(rev1);
-
-    if(ori != rev2){
-        return false;
-    }
-    return true;
-}
-
 int main(){
     int t;
     cin>>t;
 
     while(t--){
-        long long n;
+        string n;
         cin>>n;
-
-        if(checkPudding(n) == true){
-            cout<<"1"<<endl;
+        int len = n.length()-1;
+        if(n[len] == '0'){
+            cout<<"0"<<endl;
         }
         else{
-            cout<<"0"<<endl;
+            cout<<"1"<<endl;
         }
     }
 return 0;
