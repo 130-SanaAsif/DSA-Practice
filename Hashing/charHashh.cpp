a58253550a6b80adca8c822ea71c74d5935c7f74
Sana Asif <sana98asif@gmail.com>
2024-04-21 20:29:12 +0530

    My repo.

---
+++ Hashing/charHashh.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    //int hash[256] = {0};
+    int hash[26] = {0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]-'a']++;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<"Frequency Number - "<<hash[c-'a']<<endl;
+    }
+return 0;
+}
