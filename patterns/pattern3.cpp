bdc6cb079a9b01ace2476d486c87573dd406fd13
Sana Asif <sana98asif@gmail.com>
2024-07-09 21:14:41 +0530

    My repo.

--- patterns/pattern3.cpp
+++ patterns/pattern3.cpp
@@ -1,4 +1,4 @@
-//Patter 3.
+//Pattern 3.
 //1 
 //1 2
 //1 2 3
@@ -6,14 +6,18 @@
 //1 2 3 4 5
 #include<iostream>
 using namespace std;
+
 int main(){
     int n;
-    cout<<"Enter the value of n: ";
+    cout<<"Enter the value of n = ";
     cin>>n;
+
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
+        for(int j=1; j<=i; j++){            
             cout<<j<<" ";
         }
         cout<<endl;
     }
+
+    return 0;
 }
